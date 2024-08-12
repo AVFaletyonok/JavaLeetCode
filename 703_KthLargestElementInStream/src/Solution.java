68c1a28ff9b45547b2a62020d659a61bdabb29c6
Alexander <AVFaletyonok@yandex.ru>
2024-08-12 14:43:11 +0300

    master Task 703: fix task text

--- 703_KthLargestElementInStream/src/Solution.java
+++ 703_KthLargestElementInStream/src/Solution.java
@@ -6,3 +6,18 @@ public class Solution {
 
     }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val); */
+class KthLargest {
+
+    public KthLargest(int k, int[] nums) {
+
+    }
+
+    public int add(int val) {
+
+    }
+}
