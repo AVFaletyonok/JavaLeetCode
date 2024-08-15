8f47b0ea842fe98c5d542164d31eabf2f56a17e4
Alexander <AVFaletyonok@yandex.ru>
2024-08-15 14:07:13 +0300

    develop Task 860: First commit

---
+++ 860_LemonadeChange/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+
+    }
+
+    private boolean lemonadeChange(int[] bills) {
+        return false;
+    }
+}
