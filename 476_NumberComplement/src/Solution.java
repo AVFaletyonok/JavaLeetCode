f237ae7eaf88856b4bc2655e57a7c915281f6049
Alexander <AVFaletyonok@yandex.ru>
2024-08-22 12:25:32 +0300

    master Task 476: first commit

---
+++ 476_NumberComplement/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+        int complNumber = findComplement(5);
+        System.out.println("The complement of 5 is : " + complNumber);
+
+        complNumber = findComplement(1);
+        System.out.println("The complement of 1 is : " + complNumber);
+    }
+
+    private static int findComplement(int num) {
+        // code
+        return 0;
+    }
+}
