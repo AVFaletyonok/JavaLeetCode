bc599c39b2d5a9df1bbea83f9027d40af71b047b
Alexander <AVFaletyonok@yandex.ru>
2024-09-02 00:35:56 +0300

    master Task 2022 : delete solution

--- 2022_Convert1DArrayInto2DArray/src/main/java/org/example/Solution.java
+++ 2022_Convert1DArrayInto2DArray/src/main/java/org/example/Solution.java
@@ -13,13 +13,7 @@ public class Solution {
     }
 
     public static int[][] construct2DArray(int[] original, int m, int n) {
-        if (m < 1 || n < 1 || m * n != original.length) return new int[0][0];
-        int[][] resultArray = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                resultArray[i][j] = original[i * n + j];
-            }
-        }
-        return resultArray;
+
+        return new int[0][0];
     }
 }
